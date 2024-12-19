4975207739af9f065f85d023a1f8e91c1dc9c56e
SmartBagel <99964336+SmartBagel@users.noreply.github.com>
2024-12-19 15:06:43 +0100

    Fix singlequote linting issue

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Yue Tong");
+    expect(document.title).toBe('Yue Tong');
   });
 
   it('can navigate to /about', async () => {
